04118f38fbabd62b173aaa731bcb83689b934087
PetersonPriit <priit.peterson.1@ut.ee>
2024-12-11 14:58:46 +0200

    Added 2 new datasets and changed the data loading to use airflow

--- project/data/load_data_to_duckdb.py
+++ project/data/load_data_to_duckdb.py
@@ -1,3 +1,6 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
 import os
 import pandas as pd
 import duckdb
@@ -40,23 +43,25 @@ def clean_and_load(sheet_name, header_row, data_start_row, data_end_row, table_n
 # File paths for each dataset
 file_path_new = 'data/Eesti haldus- ja asustusjaotuse klassifikaator 2024v2.xlsx'
 file_path_last = 'data/PA107_20241206-142137.csv'
-input_file_path = 'data/Ametnike_palgad.xlsx'
+input_file_path_2023 = 'data/Ametnike_palgad_2023.xlsx'
+input_file_path_2022 = 'data/Ametnike_palgad_2022.xlsx'
+input_file_path_2021 = 'data/Ametnike_palgad_2021.xlsx'
 
 # Clean and load data from the first Excel file (Eesti haldus- ja asustusjaotuse klassifikaator 2024v2.xlsx)
 clean_and_load('Sheet1', 0, 1, 4802, 'klassifikaatorid', file_path_new)
 
 # Clean and load data from the second Excel file (Ametnike_palgad.xlsx)
-clean_and_load('KOV 01.04.2024 põhipalk', 7, 9, 3369, 'kov_2024', input_file_path)
-clean_and_load('KOV kogupalk 2023', 8, 10, 3970, 'kov_kogupalk_2023', input_file_path)
-clean_and_load('RIIK 01.04.2024 põhipalk', 3, 5, 11367, 'riik_2024', input_file_path)
-clean_and_load('RIIK kogupalk 2023', 6, 8, 15143, 'riik_kogupalk_2023', input_file_path)
-
+clean_and_load('KOV kogupalk 2023', 8, 10, 3970, 'kov_kogupalk_2023', input_file_path_2023)
+clean_and_load('RIIK kogupalk 2023', 6, 8, 15143, 'riik_kogupalk_2023', input_file_path_2023)
+clean_and_load('KOV_kogupalk 2022', 8, 10, 3971, 'kov_kogupalk_2022', input_file_path_2022)
+clean_and_load('RIIK_kogupalk 2022', 7, 9, 13064, 'riik_kogupalk_2022', input_file_path_2022)
+clean_and_load('KOV_kogupalk 2021', 7, 9, 3807, 'kov_kogupalk_2021', input_file_path_2021)
+clean_and_load('RIIK_kogupalk 2021', 7, 9, 13049, 'riik_kogupalk_2021', input_file_path_2021)
 # Clean and load data from the CSV file (PA107_20241206-142137.csv)
 keskmised_näitajad = pd.read_csv(file_path_last, header=2)  # Loading CSV with header_row_last = 2
-keskmised_näitajad_cleaned = keskmised_näitajad.iloc[3:76, :]  # Slice the relevant rows (data only)
 
 # Register the DataFrame as a temporary table in DuckDB
-con.register('temp_keskmised_näitajad', keskmised_näitajad_cleaned)
+con.register('temp_keskmised_näitajad', keskmised_näitajad)
 
 # Create a table in DuckDB from the registered DataFrame
 table_name_keskmised_näitajad = "keskmised_näitajad"  # Table name in DuckDB
